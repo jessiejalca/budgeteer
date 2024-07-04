5025093c8721d0fa9584ac4afc7ea48d522eecaf
Jessie Jalca <jessiejalca@gmail.com>
2024-07-04 17:47:47 -0400

    style: reset css for simplified future styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 import Calculator from "./components/Calculator";
 
 function App() {
 
   return (
     <>
-      <hgroup>
-        <h1>Budgeteer.</h1>
-        <p className="subhead">Let's calculate stuff.</p>
-      </hgroup>
-
+      <Header />
       <Calculator />
     </>
   );
