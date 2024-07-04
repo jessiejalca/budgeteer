f37b7562b9e8db726ff521d5032b1c9d7a4c67ec
Jessie Jalca <jessiejalca@gmail.com>
2024-07-04 17:48:18 -0400

    refactor: move Header to separate component
    NOTE
    Accidentally staged App component in previous commit without Header
    component, creating a rendering error. This commit resolves the issue.

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <hgroup>
+      <h1>Budgeteer.</h1>
+      <p className="subhead">Let's calculate stuff.</p>
+    </hgroup>
+  );
+}
+
+export default Header;
