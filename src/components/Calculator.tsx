464be68b9f04dbd533db5223664b28d9137929d1
Jessie Jalca <jessiejalca@gmail.com>
2024-07-04 22:17:52 -0400

    feat: collect all form data

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -3,6 +3,16 @@ import React from "react";
 const Calculator = () => {
     const [savingsGoal, setSavingsGoal] = React.useState<string>("");
     const [goalAmount, setGoalAmount] = React.useState<number>(0);
+    const [amountSaved, setAmountSaved] = React.useState<number>(0);
+    const [fundingFrequency, setFundingFrequency] = React.useState<string>("weekly");
+
+    // Set the default date to today
+    const today: string = new Date().toISOString().split("T")[0];
+    const [targetDate, setTargetDate] = React.useState<string>(today);
+
+    const handleFrequencyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setFundingFrequency(e.target.value);
+    };
 
     return (
     <>
@@ -24,12 +34,12 @@ const Calculator = () => {
           />
         </label>
         <label>
-          <span>How much do you have set aside for this goal already?</span>
-          <input type="number" />
+          <span>How much do you have already?</span>
+          <input type="number" value={amountSaved} onChange={(e) => setAmountSaved(Number(e.target.value))} />
         </label>
         <label>
           <span>How often do you want to fund this goal?</span>
-          <select>
+          <select value={fundingFrequency} onChange={handleFrequencyChange}>
             <option value="weekly">Weekly</option>
             <option value="bi-weekly">Bi-weekly</option>
             <option value="monthly">Monthly</option>
@@ -37,13 +47,13 @@ const Calculator = () => {
         </label>
         <label>
           <span>What's your target date?</span>
-          <input type="date" />
+          <input type="date" value={targetDate} onChange={(e) => setTargetDate(e.target.value)} min={today} />
         </label>
         <button type="submit">Calculate</button>
       </form>
-      {savingsGoal && (
+      {(
         <p>
-          You need ${goalAmount} to meet your {savingsGoal} goal.
+          You need ${goalAmount} {fundingFrequency} to meet your {savingsGoal} goal by {targetDate}.
         </p>
       )}
     </>
