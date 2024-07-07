29946b1af8103a24b547585c0887e0c2c8ada386
Jessie Jalca <jessiejalca@gmail.com>
2024-07-07 00:18:14 -0400

    feat: reformat form to read as single sentence fill-in-the-blank

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Results from "./Results";
 
 const Calculator = () => {
-  const [showResults, setShowResults] = React.useState<boolean>(false);
   const [savingsGoal, setSavingsGoal] = React.useState<string>("");
   const [goalAmount, setGoalAmount] = React.useState<number>(0);
   const [amountSaved, setAmountSaved] = React.useState<number>(0);
-  const [fundingFrequency, setFundingFrequency] = React.useState<string>("weekly");
+  const [fundingFrequency, setFundingFrequency] =
+    React.useState<string>("weekly");
   const [amountPerPeriod, setAmountPerPeriod] = React.useState<number>(0);
 
   // Set the default date to today
@@ -14,24 +13,31 @@ const Calculator = () => {
   const [targetDate, setTargetDate] = React.useState<string>(today);
 
   // Get the value of the selected frequency
-  const handleFrequencyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleFrequencyChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
     setFundingFrequency(e.target.value);
   };
 
   // Handle form submission: show results
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    
+
     // Get the amount needed per funding period
-    const amountPerPeriod: number = calculateAmountPerPeriod(goalAmount, calculatePeriods(targetDate, fundingFrequency));
+    const amountPerPeriod: number = calculateAmountPerPeriod(
+      goalAmount,
+      calculatePeriods(targetDate, fundingFrequency)
+    );
 
     // Update the state with the amount per period and show the results
     setAmountPerPeriod(amountPerPeriod);
-    setShowResults(true);
-  }
+  };
 
   // Calculate the number of funding periods
-  const calculatePeriods = (targetDate: string, fundingFrequency: string) => {
+  const calculatePeriods = (
+    targetDate: string,
+    fundingFrequency: string
+  ): number => {
     const today: Date = new Date();
     const target: Date = new Date(targetDate);
     const difference: number = target.getTime() - today.getTime();
@@ -57,58 +63,67 @@ const Calculator = () => {
   };
 
   // Calculate the amount needed per funding period
-  const calculateAmountPerPeriod = (goalAmount: number, periods: number) => {
+  const calculateAmountPerPeriod = (
+    goalAmount: number,
+    periods: number
+  ): number => {
     return Math.ceil(goalAmount / periods);
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <label>
-          <span>What are you saving for?</span>
-          <input
-            type="text"
-            value={savingsGoal}
-            onChange={(e) => setSavingsGoal(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>How much do you need?</span>
-          <input
-            type="number"
-            value={goalAmount}
-            onChange={(e) => setGoalAmount(Number(e.target.value))}
-          />
-        </label>
-        <label>
-          <span>How much do you have already?</span>
-          <input type="number" value={amountSaved} onChange={(e) => setAmountSaved(Number(e.target.value))} />
-        </label>
-        <label>
-          <span>How often do you want to fund this goal?</span>
-          <select value={fundingFrequency} onChange={handleFrequencyChange}>
-            <option value="weekly">Weekly</option>
-            <option value="bi-weekly">Bi-weekly</option>
-            <option value="monthly">Monthly</option>
-          </select>
-        </label>
-        <label>
-          <span>What's your target date?</span>
-          <input type="date" value={targetDate} onChange={(e) => setTargetDate(e.target.value)} min={today} />
-        </label>
+        <p>
+          With ${/* amount already saved */}
+          <label>
+            <input
+              type="number"
+              value={amountSaved}
+              onChange={(e) => setAmountSaved(Number(e.target.value))}
+            />
+          </label>{" "}
+          already saved, you need to contribute
+          {/* amount calculated */}{" "}
+          <span className="calc-value">${amountPerPeriod}</span>{" "}
+          {/* funding frequency */}
+          <label>
+            <select value={fundingFrequency} onChange={handleFrequencyChange}>
+              <option value="weekly">weekly</option>
+              <option value="bi-weekly">bi-weekly</option>
+              <option value="monthly">monthly</option>
+            </select>
+          </label>{" "}
+          to meet your {/* savings goal name */}
+          <label>
+            <input
+              type="text"
+              placeholder="vacation"
+              value={savingsGoal}
+              onChange={(e) => setSavingsGoal(e.target.value)}
+            />
+          </label>{" "}
+          goal of ${/* goal amount */}
+          <label>
+            <input
+              type="number"
+              value={goalAmount}
+              onChange={(e) => setGoalAmount(Number(e.target.value))}
+            />
+          </label>{" "}
+          by
+          {/* goal target date */}
+          <label>
+            <input
+              type="date"
+              value={targetDate}
+              onChange={(e) => setTargetDate(e.target.value)}
+              min={today}
+            />
+          </label>
+          .
+        </p>
         <button type="submit">Calculate</button>
       </form>
-      
-      {showResults && (
-        <Results
-          goalAmount={goalAmount}
-          fundingFrequency={fundingFrequency}
-          savingsGoal={savingsGoal}
-          targetDate={targetDate}
-          amountSaved={amountSaved}
-          amountPerPeriod={amountPerPeriod}
-        />
-      )}
     </>
   );
 };
