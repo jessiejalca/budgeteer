f4bc8d20ec079d294639dfafcd3f39896c6b39e6
Jessie Jalca <jessiejalca@gmail.com>
2024-07-08 21:34:09 -0400

    fix: Add TargetAmount component to fix missing component issue

---
+++ src/components/TargetAmount.tsx
@@ -0,0 +1,14 @@
+// import React from "react";
+
+const TargetAmount = () => {
+  return (
+    <div role="tabpanel" id="target-amount">
+      <p>
+        If I put $100 towards my savings goal every month starting today, I will
+        reach my goal of $1000 in 10 months, by 2022-12-01.
+      </p>
+    </div>
+  );
+};
+
+export default TargetAmount;
